cde2dd6df63729bdd85df902215917eb14419d92
Dewes <rdewes@gmail.com>
2021-08-06 16:07:56 -0300

    updates the version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name = 'airflow-sqlcmd-operator',         
     packages = ['airflow_sqlcmd_operator'],   
-    version = '0.1',      
+    version = '0.2',      
     license='MIT',        
     description = 'Custom Airflow BashOperator for Microsoft sqlcmd',   
     author = 'Rodrigo Dewes',                   
     author_email = 'rdewes@gmail.com',      
     url = 'https://github.com/dewes/airflow-sqlcmd-operator',   
-    download_url = 'https://github.com/dewes/airflow-sqlcmd-operator/archive/refs/tags/v_01.tar.gz',    # I explain this later on
+    download_url = 'https://github.com/dewes/airflow-sqlcmd-operator/archive/refs/tags/v_02.tar.gz',    # I explain this later on
     keywords = ['Airflow', 'operator', 'SQLServer', 'sqlcmd'],   
     install_requires=[
             'apache-airflow',            
