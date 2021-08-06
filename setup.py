de7570b6b3a068cebb797b0f6c0f82ca4b9c69a6
Dewes <rdewes@gmail.com>
2021-08-06 18:35:52 -0300

    updates to tag v_03

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author = 'Rodrigo Dewes',                   
     author_email = 'rdewes@gmail.com',      
     url = 'https://github.com/dewes/airflow-sqlcmd-operator',   
-    download_url = 'https://github.com/dewes/airflow-sqlcmd-operator/archive/refs/tags/v_02.tar.gz',
+    download_url = 'https://github.com/dewes/airflow-sqlcmd-operator/archive/refs/tags/v_03.tar.gz',
     keywords = ['Airflow', 'operator', 'SQLServer', 'sqlcmd'],   
     install_requires=[
             'apache-airflow>=2.1',            
