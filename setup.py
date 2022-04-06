3ff63b1c0f0e96276e616e163207aac16521c635
rodrigod <rodrigo.dewes@cgu.gov.br>
2022-04-06 18:12:49 -0300

    adjusted version and requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 setup(
     name="airflow-sqlcmd-operator",
     packages=["airflow_sqlcmd_operator"],
-    version="0.5",
+    version="0.5.1",
     license="MIT",
     description="Custom Airflow BashOperator for Microsoft sqlcmd",
     long_description=README,
